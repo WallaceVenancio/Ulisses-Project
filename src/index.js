d98ba822a9d38e1c6e8516b536e052428fef87e9
Wallace Venancio <wallacefvenancio@gmail.com>
2026-04-01 16:55:05 -0300

    Adição do botão para liberar o acesso

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 alert("Volte aos estudos!");
 
 const novaCaixa = document.createElement('div');
-
-novaCaixa.innerText = 'PARE DE ASSISTIR VÍDEOS E VOLTE AOS ESTUDOS!';
-
+novaCaixa.innerText = 'PARE DE ASSISTIR VÍDEOS E VOLTE AOS ESTUDOS!\n\n';
 novaCaixa.classList.add('bloqueio-estudos');
 
-document.body.appendChild(novaCaixa);
+const botaoSair = document.createElement('button');
+botaoSair.innerText = 'Entendi, vou estudar!';
+botaoSair.classList.add('meu-botao-estilo'); 
 
+botaoSair.onclick = function() {
+    novaCaixa.remove(); 
+    document.body.classList.remove('sem-scroll'); 
+};
+
+novaCaixa.appendChild(botaoSair);
+
+document.body.appendChild(novaCaixa);
 document.body.classList.add('sem-scroll');
