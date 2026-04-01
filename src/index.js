9a37ba600559d311fd12c828afdbe65e76c21d69
Wallace Venancio <wallacefvenancio@gmail.com>
2026-04-01 11:52:48 -0300

    Adição do css e a alteração da pagina não acontece mais no head do html, mas uma div é criada com uma caixa com texto

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-alert("Volte aos estudos")
-document.head.innerHTML = "Volte aos estudos";
-document.body.innerHTML = "Volte aos estudos";
+alert("Volte aos estudos!");
 
+const novaCaixa = document.createElement('div');
+
+novaCaixa.innerText = 'PARE DE ASSISTIR VÍDEOS E VOLTE AOS ESTUDOS!';
+
+novaCaixa.classList.add('bloqueio-estudos');
+
+document.body.appendChild(novaCaixa);
+
+document.body.classList.add('sem-scroll');
